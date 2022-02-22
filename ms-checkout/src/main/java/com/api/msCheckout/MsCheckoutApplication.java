6dc19f30f7e40ead5bf021c4a0c4cbbe8fb9e41a
duartesluiza <duartes.luiza92@gmail.com>
2022-02-22 08:30:37 -0300

    Subindo ms-checkout version 1

---
+++ ms-checkout/src/main/java/com/api/msCheckout/MsCheckoutApplication.java
@@ -0,0 +1,13 @@
+package com.api.msCheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCheckoutApplication.class, args);
+	}
+
+}
---
+++ ms-checkout/src/test/java/com/api/msCheckout/MsCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.msCheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
