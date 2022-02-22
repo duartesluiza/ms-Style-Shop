8155e4299e4517ef14da96469c5166a83491beb3
duartesluiza <duartes.luiza92@gmail.com>
2022-02-22 11:27:04 -0300

    Subindo ms-catalog version 1

--- ms-checkout/src/main/java/com/api/msCheckout/MsCheckoutApplication.java
+++ ms-checkout/src/main/java/com/api/msCheckout/MsCheckoutApplication.java
@@ -2,8 +2,10 @@ package com.api.msCheckout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MsCheckoutApplication {
 
 	public static void main(String[] args) {
